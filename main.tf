082112284fefe45b91303675c5c35628016f1c80
Mita0106 <98621413+Mita0106@users.noreply.github.com>
2024-03-18 21:58:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "RHEL-Server" {
+resource "aws_instance" "RHEL-vm-server" {
   count         = 1
   ami           = "ami-0fe630eb857a6ec83"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   key_name      = "Mita2_keypair"
   tags = {
     Name = "RHEL-Server"
