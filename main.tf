aa796a23aa9956874afc921b5ddaf4df16c4fb43
Mita0106 <98621413+Mita0106@users.noreply.github.com>
2024-03-18 21:34:34 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "RHEL-Server" {
   count         = 1
   ami           = "ami-0fe630eb857a6ec83"
   instance_type = "t2.micro"
-  key_name      = "Mita2_keyPair"
+  key_name      = "Mita2_keypair"
   tags = {
     Name = "RHEL-Server"
   }
